28e4960f194361c4e8179f99fa30332874f89fe2
William Donovan-Seid <wdstwo@live.com>
2020-08-19 11:44:44 -0700

    complete fourth TDD
    co-author: Brycen Bartolome <brycenbartolome@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,25 @@ let word="apple";
 let newWord = word + "way";
 console.log(newWord);
 
-const vowels = ["a", "i", "e", "o", "u"];
+let vowels = ["u", "a", "i", "e", "o"];
 const word = "house";
 let stringArray = word.split("");
-if (stringArray[0] !== "y") {
-	vowels.push("y")
-}
-for (let index=0; index < stringArray.length; index+=1) {
-	if (vowels.includes(stringArray[0])) {
-  	break;
-  } else {
-  	stringArray.push(stringArray[0]);
-    stringArray.shift();
-  }  
+if (vowels.includes(stringArray[0])) {
+  stringArray.push("w")
+} else {
+  if (stringArray[0] !== "y") {
+    vowels.push("y")
+  }
+  if (stringArray[0] === "q" && stringArray[1] === "u") {
+    vowels.shift();
+  }
+  for (let index=0; index < stringArray.length; index+=1) {
+    if (vowels.includes(stringArray[0])) {
+      break;
+    } else {
+      stringArray.push(stringArray[0]);
+      stringArray.shift();
+    }  
+  }
 }
 const newWord = stringArray.join("") + "ay";
