29c4c0423598aa41257658ab5df8878f9734c83e
William Donovan-Seid <wdstwo@live.com>
2020-08-19 11:59:17 -0700

    compile all the TDDS and add html stuff
    co-author: Brycen Bartolome <brycenbartolome@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,28 @@
-let word="apple";
-let newWord = word + "way";
-console.log(newWord);
-
-let vowels = ["u", "a", "i", "e", "o"];
-const word = "house";
-let stringArray = word.split("");
-if (vowels.includes(stringArray[0])) {
-  stringArray.push("w")
-} else {
-  if (stringArray[0] !== "y") {
-    vowels.push("y")
-  }
-  if (stringArray[0] === "q" && stringArray[1] === "u") {
-    vowels.shift();
-  }
-  for (let index=0; index < stringArray.length; index+=1) {
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+    let vowels = ["u", "a", "i", "e", "o"];
+    const word = $("#word").val();
+    let stringArray = word.split("");
     if (vowels.includes(stringArray[0])) {
-      break;
+      stringArray.push("w");
     } else {
-      stringArray.push(stringArray[0]);
-      stringArray.shift();
-    }  
-  }
-}
-const newWord = stringArray.join("") + "ay";
+      if (stringArray[0] !== "y") {
+        vowels.push("y");
+      }
+      if (stringArray[0] === "q" && stringArray[1] === "u") {
+        vowels.shift();
+      }
+      for (let index=0; index < stringArray.length; index+=1) {
+        if (vowels.includes(stringArray[0])) {
+          break;
+        } else {
+          stringArray.push(stringArray[0]);
+          stringArray.shift();
+        }  
+      }
+    }
+    const newWord = stringArray.join("") + "ay";
+    $("#answer").text(newWord);
+  });
+});
