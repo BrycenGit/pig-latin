064934676fd2d2015863239ff5ba3db68851c69e
William Donovan-Seid <wdstwo@live.com>
2020-08-19 11:33:43 -0700

    complete third TDD
    co-author: Brycen Bartolome <brycenbartolome@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,9 @@ console.log(newWord);
 const vowels = ["a", "i", "e", "o", "u"];
 const word = "house";
 let stringArray = word.split("");
+if (stringArray[0] !== "y") {
+	vowels.push("y")
+}
 for (let index=0; index < stringArray.length; index+=1) {
 	if (vowels.includes(stringArray[0])) {
   	break;
